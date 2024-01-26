ff62581c688da9f98ac85a8b502e29d5b113ca84
Milkias Tonji <tongisttonji@gmail.com>
2024-01-26 15:46:53 +0300

    initial commit with the models and data insertion

---
+++ my_tennis_club/members/views.py
@@ -0,0 +1,18 @@
+from django.template import loader
+from django.http import HttpResponse
+
+# importing models
+from .models import Member
+
+# Create your views here.
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
+
+def staffs(request):
+    template = loader.get_template("staffs.html")
+    return HttpResponse(template.render())
